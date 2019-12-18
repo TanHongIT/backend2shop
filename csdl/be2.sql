ede9037eeac0e10bef004f2563ca4d07cfcc3c08
Thuận Trần <thuankg1752@gmail.com>
2019-12-18 13:06:50 +0700

    Update be2.sql

--- csdl/be2.sql
+++ csdl/be2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1:3306
--- Thời gian đã tạo: Th12 18, 2019 lúc 03:32 AM
+-- Thời gian đã tạo: Th12 18, 2019 lúc 06:06 AM
 -- Phiên bản máy phục vụ: 5.7.26
 -- Phiên bản PHP: 7.2.18
 
@@ -173,7 +173,22 @@ INSERT INTO `category_product` (`product_id`, `category_id`) VALUES
 (57, 8),
 (57, 13),
 (58, 8),
-(58, 13);
+(58, 13),
+(60, 1),
+(60, 3),
+(60, 7),
+(61, 1),
+(62, 1),
+(62, 3),
+(62, 7),
+(63, 3),
+(63, 4),
+(63, 6),
+(64, 3),
+(64, 4),
+(64, 6),
+(65, 5),
+(66, 5);
 
 -- --------------------------------------------------------
 
@@ -292,13 +307,6 @@ CREATE TABLE IF NOT EXISTS `products` (
 --
 
 INSERT INTO `products` (`id`, `product_name`, `product_price`, `product_description`, `product_image`, `product_promotion_pricre`) VALUES
-(1, 'Máy tính bảng iPad WiFi 32GB New 2018 - Hàng Chính Hãng', '6980000.00', 'Thần thái của tablet cao cấp bên mức giá vừa phải\r\niPad WiFi 32GB New 2018 vẫn giữ phong cách thiết kế quen thuộc như phiên bản tiền nhiệm 2017 với chất liệu nhôm nguyên khối cao cấp, các cạnh được bo cong mềm mại, tạo cảm giác cầm nắm thoải mái và chắc tay. Bên cạnh đó các chi tiết đều được gia công một cách tỉ mỉ và tinh tế, bạn sẽ phải \"Wow\" lên vì sức hút mãnh liệt từ vẻ đẹp bên ngoài của nó.', 'iPadWiFi32GB.jpg', '0.00'),
-(2, 'Máy đọc sách Kindle PaperWhite 2018 gen 4 (10th) - Bản 8GB 2019 - Hàng chính hãng', '2887000.00', 'Thiết kế mỏng nhẹ\r\nMáy đọc sách Kindle PaperWhite 2018 gen 4 (10th) - Bản 8 GB - Hàng chính hãng được thiết kế cực kỳ mỏng, nhẹ và màn hình sáng hơn. Trọng lượng của máy là 182 gram và dày 8,18 mm, so với phiên bản trước là 200 gram và 9 mm. Màn hình của Paperwhite vẫn là 6 inch và 300 ppi, được Vergeđánh giá xuất sắc và không có gì để phàn nàn.', 'KindlePaperWhite.jpg', '0.00'),
-(3, 'Máy tính bảng iPad Mini 5 Wi-Fi 64GB - Hàng Chính Hãng', '9350000.00', 'Thiết kế mỏng, gọn nhẹ\r\niPad Mini 5 Wi-Fi 64GB được thiết kế tinh tế, sang trọng với vỏ nhôm tạo cảm giác cầm chắc tay. Máy tính bảng có kích thước siêu mỏng với độ dày chỉ 6.1mm và trọng lượng siêu nhẹ khoảng 300g giúp bạn dễ dàng bỏ vào túi xách để mang theo mọi lúc mọi nơi, đáp ứng đủ các nhu cầu cho cuộc sống không ngừng chuyển động của bạn.', 'ipadmini.jpg', '0.00'),
-(4, 'Điện Thoại iPhone 7 Plus 32GB - Hàng Chính Hãng', '11990000.00', 'Thiết kế kim loại nguyên khối sang trọng\r\nĐiện Thoại iPhone 7 Plus 32GB - Hàng Chính Hãng FPT với kích thước 158.2 x 77.9 x 7.3 mm mỏng nhẹ và thiết kế tương tự như bộ đôi iPhone 6s/6s Plus, tuy nhiên phần dải nhựa bắt sóng không cắt ngang mặt lưng như phiên bản cũ mà được chuyển sang phần cạnh trên của sản phẩm. Phím Home vật lý trên điện thoại cũng được thay thế bằng phím cảm ứng nhờ vào sự kết hợp Taptic Engine mới và liên kết với 3D Touch tiện lợi và đẹp mắt.', 'iphone7plus.jpg', '0.00'),
-(5, 'Điện Thoại iPhone X 64GB VN/A - Hàng Chính Hãng', '20190000.00', 'Thiết kế lạ mắt không nút Home cứng\r\nĐiện Thoại iPhone X 64GB là chiếc điện thoại hoàn toàn mới của Apple vừa mới ra mắt tuần vừa qua. Trên cơ bản, iPhone X vẫn có những tính năng như những dòng iPhone khác nhưng thiết kế bên ngoài lạ mắt hơn, không trang bị nút Home cứng, viền kim loại sang trọng và đặc biệt là cụm camera sau được trang bị theo chiều dọc tạo điểm nhấn cho chiếc điện thoại.', 'iphonex.jpg', '0.00'),
-(6, 'Điện Thoại Samsung Galaxy A70 (128GB/6GB) - Hàng Chính Hãng', '7189000.00', 'Điện Thoại Samsung Galaxy A70 (128GB/6GB) - Hàng Chính Hãng (Đã Kích Hoạt) Bảo Hành 12 Tháng sản phẩm vẫn được làm bằng chất liệu nhựa giả thủy tinh nhưng là nhựa cao cấp với tên gọi 3D Graffitistic, mang đến sự cứng cáp và chắc chắn khi cầm nắm.\r\n\r\nBên cạnh đó, màu sắc trên lưng máy được trang bị thêm hiệu ứng lấp lánh nên khi nhìn theo góc nghiêng sẽ rất đẹp mắt. Đáng tiếc là A70 vẫn bị bám mồ hôi và dấu vân tay. Ngoài ra, viền bezel của máy cũng được làm rất mỏng, so với các máy thuộc dòng Galaxy A thì A70 là mỏng nhất.', 'SamsungGalaxyA70.jpg', '0.00'),
-(7, 'Điện thoại Samsung Galaxy M10 Ram 2GB 16GB - Hàng chính hãng', '2580000.00', 'Màn hình: PLS TFT LCD, 6.2\", HD+ • Hệ điều hành: Android 8.1 (Oreo) • Camera sau: Chính 13 MP & Phụ 5 MP • Camera trước: 5 MP • CPU: Exynos 7870 8 nhân 64-bit • RAM: 2 GB • Bộ nhớ trong: 16 GB • Thẻ nhớ: MicroSD • Thẻ SIM: 2 SIM Nano (SIM 2 chung khe thẻ nhớ) • Dung lượng pin: 3400 mAh', 'SamsungGalaxyM10.png', '0.00'),
 (38, 'Laptop ASUS ZenBook 13 UX333FA-A4011T (13.3\" FHD/i5-8265U/8GB/256GB SSD/UHD 620/Win10/1.2 kg)', '22990000.00', 'Tên sản phẩm: Máy tính xách tay/ Laptop Asus Zenbook UX333FA-A4011T (I5-8265U) (Xanh)\r\n\r\n- CPU: Intel Core i5-8265U ( 1.6 GHz - 3.9 GHz / 6MB / 4 nhân, 8 luồng )\r\n- Màn hình: 13.3\" ( 1920 x 1080 ) , không cảm ứng\r\n- RAM: 8GB Onboard LPDDR3\r\n- Đồ họa: Intel UHD Graphics 620\r\n- Lưu trữ: 256GB SSD M.2 NVMe\r\n- Hệ điều hành: Windows 10 Home SL 64-bit\r\n- Pin: 3 cell 50 Wh Pin liền , khối lượng: 1.2 kg', 'Asus_Zenbook_UX333_RoyalBlue_NoNumpad_6.jpg', '22490000.00'),
 (39, 'Laptop MSI GL65 9SDK-054VN (15\" FHD/i5-9300H/8GB/512GB SSD/GTX 1660Ti/Win10/2.3 kg)', '29490000.00', '- CPU: Intel Core i5 9300H (2.4 GHz - 4.1 GHz/8MB/4 nhân, 8 luồng)\r\n- Màn hình: 15.6\" IPS (1920 x 1080), không cảm ứng\r\n- RAM: 1 x 8GB DDR4 2666MHz (2 Khe cắm, Hỗ trợ tối đa 64GB)\r\n- Đồ họa: Intel UHD Graphics 630/ NVIDIA GeForce GTX 1660Ti 6GB\r\n- Lưu trữ: 512GB SSD M.2 NVMe /\r\n- Hệ điều hành: Windows 10 Home SL 64-bit\r\n- Pin: 6 cell 51 Wh Pin liền, Khối lượng: 2.3 kg', 'MSI_GL65_1.jpg', '29090000.00'),
 (40, 'Laptop MSI Prestige PS42 Modern 8M-288VN (14\" FHD/i5-8250U/8GB/UHD 620/Win10/1.2 kg)', '21960000.00', '- CPU: Intel Core i5-8250U ( 1.6 GHz - 3.4 GHz / 6MB / 4 nhân, 8 luồng )\r\n- Màn hình: 14\" ( 1920 x 1080 ) , không cảm ứng\r\n- RAM: 1 x 8GB DDR4 2666MHz\r\n- Đồ họa: Intel UHD Graphics 620 / Shared memory\r\n- Lưu trữ: 256GB SSD M.2 NVMe\r\n- Hệ điều hành: Windows 10 Home SL 64-bit\r\n- Pin: 4 cell 50 Wh Pin liền , khối lượng: 1.2 kg', 'msi ps42_8m_1.jpg', '21060000.00'),
@@ -319,7 +327,14 @@ INSERT INTO `products` (`id`, `product_name`, `product_price`, `product_descript
 (55, 'Laptop Dell Inspiron 3567-N3567U (15\" FHD/i3-7020U/4GB/1TB HDD/HD 620/Ubuntu/2.3 kg)', '9490000.00', '- CPU: Intel Core i3-7020U ( 2.3 GHz / 3MB / 2 nhân, 4 luồng )\r\n- Màn hình: 15.6\" TN ( 1920 x 1080 ) , không cảm ứng\r\n- RAM: 1 x 4GB DDR4 2400MHz\r\n- Đồ họa: Intel HD Graphics 620 / Shared memory\r\n- Lưu trữ: 1TB HDD 5400RPM\r\n- Hệ điều hành: Ubuntu\r\n- Pin: 4 cell Pin rời , khối lượng: 2.3 kg', 'dell inspiron 3567_2.jpg', '9090000.00'),
 (56, 'Laptop Acer Swift 3 SF314-57G-53T1 (NX.HJESV.001) (14\" FHD/i5-1035G1/8GB/512GB SSD/MX250/Win10/1.2kg)\r\n', '19990000.00', '- CPU: Intel Core i5-1035G1 (1.0 GHz - 3.6 GHz/6MB/4 nhân, 8 luồng)\r\n- Màn hình: 14\" IPS (1920 x 1080), không cảm ứng\r\n- RAM: 8GB Onboard LPDDR4 2666MHz , Không nâng cấp được)\r\n- Đồ họa: Intel UHD Graphics/ NVIDIA GeForce MX250 2GB\r\n- Lưu trữ: 512GB SSD M.2 NVMe /\r\n- Hệ điều hành: Windows 10 Home 64-bit\r\n- Pin: 3 cell 48 Wh Pin liền, Khối lượng: 1.2 kg', 'Acer_Swift_3_SF314-57G_Gray_3.jpg', '19490000.00'),
 (57, 'Laptop Lenovo ThinkPad E590 (20NBS07000) (15\" HD/i5-8265U/4GB/1TB HDD/UHD 620/Free DOS/2.1 kg)\r\n', '16990000.00', '- CPU: Intel Core i5-8265U ( 1.6 GHz - 3.9 GHz / 6MB / 4 nhân, 8 luồng )\r\n- Màn hình: 15.6\" IPS ( 1366 x 768 ) , không cảm ứng\r\n- RAM: 1 x 4GB DDR4 2400MHz\r\n- Đồ họa: Intel UHD Graphics 620\r\n- Lưu trữ: 1TB HDD 5400RPM\r\n- Hệ điều hành: Free DOS\r\n- Pin: 3 cell 45 Wh Pin rời , khối lượng: 2.1 kg', 'lenovo thinkpad e590_1.jpg', '16490000.00'),
-(58, 'Laptop Lenovo ThinkPad L380 (20M5S01500) (13.3\" FHD/i5-8250U/4GB/UHD 620/Free DOS/1.5 kg)\r\n', '19490000.00', '- CPU: Intel Core i5-8250U ( 1.6 GHz - 3.4 GHz / 6MB / 4 nhân, 8 luồng )\r\n- Màn hình: 13.3\" IPS ( 1920 x 1080 ) , không cảm ứng\r\n- RAM: 1 x 4GB DDR4 2400MHz\r\n- Đồ họa: Intel UHD Graphics 620\r\n- Lưu trữ: 256GB SSD M.2 SATA\r\n- Hệ điều hành: Free DOS\r\n- Pin: 3 cell 45 Wh Pin liền , khối lượng: 1.5 kg', 'thinkpad-l380-20m5s01500-i5-8250u-bac-1-v.jpg', '19090000.00');
+(58, 'Laptop Lenovo ThinkPad L380 (20M5S01500) (13.3\" FHD/i5-8250U/4GB/UHD 620/Free DOS/1.5 kg)\r\n', '19490000.00', '- CPU: Intel Core i5-8250U ( 1.6 GHz - 3.4 GHz / 6MB / 4 nhân, 8 luồng )\r\n- Màn hình: 13.3\" IPS ( 1920 x 1080 ) , không cảm ứng\r\n- RAM: 1 x 4GB DDR4 2400MHz\r\n- Đồ họa: Intel UHD Graphics 620\r\n- Lưu trữ: 256GB SSD M.2 SATA\r\n- Hệ điều hành: Free DOS\r\n- Pin: 3 cell 45 Wh Pin liền , khối lượng: 1.5 kg', 'thinkpad-l380-20m5s01500-i5-8250u-bac-1-v.jpg', '19090000.00'),
+(60, 'Máy tính bảng iPad WiFi 32GB New 2018 - Hàng Chính Hãng', '6980000.00', 'Thần thái của tablet cao cấp bên mức giá vừa phải\r\niPad WiFi 32GB New 2018 vẫn giữ phong cách thiết kế quen thuộc như phiên bản tiền nhiệm 2017 với chất liệu nhôm nguyên khối cao cấp, các cạnh được bo cong mềm mại, tạo cảm giác cầm nắm thoải mái và chắc tay. Bên cạnh đó các chi tiết đều được gia công một cách tỉ mỉ và tinh tế, bạn sẽ phải \"Wow\" lên vì sức hút mãnh liệt từ vẻ đẹp bên ngoài của nó.', 'iPadWiFi32GB.jpg', '0.00'),
+(61, 'Máy đọc sách Kindle PaperWhite 2018 gen 4 (10th) - Bản 8GB 2019 - Hàng chính hãng', '2887000.00', 'Thiết kế mỏng nhẹ\r\nMáy đọc sách Kindle PaperWhite 2018 gen 4 (10th) - Bản 8 GB - Hàng chính hãng được thiết kế cực kỳ mỏng, nhẹ và màn hình sáng hơn. Trọng lượng của máy là 182 gram và dày 8,18 mm, so với phiên bản trước là 200 gram và 9 mm. Màn hình của Paperwhite vẫn là 6 inch và 300 ppi, được Vergeđánh giá xuất sắc và không có gì để phàn nàn.', 'KindlePaperWhite.jpg', '0.00'),
+(62, 'Máy tính bảng iPad Mini 5 Wi-Fi 64GB - Hàng Chính Hãng', '9350000.00', 'Thiết kế mỏng, gọn nhẹ\r\niPad Mini 5 Wi-Fi 64GB được thiết kế tinh tế, sang trọng với vỏ nhôm tạo cảm giác cầm chắc tay. Máy tính bảng có kích thước siêu mỏng với độ dày chỉ 6.1mm và trọng lượng siêu nhẹ khoảng 300g giúp bạn dễ dàng bỏ vào túi xách để mang theo mọi lúc mọi nơi, đáp ứng đủ các nhu cầu cho cuộc sống không ngừng chuyển động của bạn.', 'ipadmini.jpg', '0.00'),
+(63, 'Điện Thoại iPhone 7 Plus 32GB - Hàng Chính Hãng', '11990000.00', 'Thiết kế kim loại nguyên khối sang trọng\r\nĐiện Thoại iPhone 7 Plus 32GB - Hàng Chính Hãng FPT với kích thước 158.2 x 77.9 x 7.3 mm mỏng nhẹ và thiết kế tương tự như bộ đôi iPhone 6s/6s Plus, tuy nhiên phần dải nhựa bắt sóng không cắt ngang mặt lưng như phiên bản cũ mà được chuyển sang phần cạnh trên của sản phẩm. Phím Home vật lý trên điện thoại cũng được thay thế bằng phím cảm ứng nhờ vào sự kết hợp Taptic Engine mới và liên kết với 3D Touch tiện lợi và đẹp mắt.', 'iphone7plus.jpg', '0.00'),
+(64, 'Điện Thoại iPhone X 64GB VN/A - Hàng Chính Hãng', '20190000.00', 'Thiết kế lạ mắt không nút Home cứng\r\nĐiện Thoại iPhone X 64GB là chiếc điện thoại hoàn toàn mới của Apple vừa mới ra mắt tuần vừa qua. Trên cơ bản, iPhone X vẫn có những tính năng như những dòng iPhone khác nhưng thiết kế bên ngoài lạ mắt hơn, không trang bị nút Home cứng, viền kim loại sang trọng và đặc biệt là cụm camera sau được trang bị theo chiều dọc tạo điểm nhấn cho chiếc điện thoại.', 'iphonex.jpg', '0.00'),
+(65, 'Điện Thoại Samsung Galaxy A70 (128GB/6GB) - Hàng Chính Hãng', '7189000.00', 'Điện Thoại Samsung Galaxy A70 (128GB/6GB) - Hàng Chính Hãng (Đã Kích Hoạt) Bảo Hành 12 Tháng sản phẩm vẫn được làm bằng chất liệu nhựa giả thủy tinh nhưng là nhựa cao cấp với tên gọi 3D Graffitistic, mang đến sự cứng cáp và chắc chắn khi cầm nắm.\r\n\r\nBên cạnh đó, màu sắc trên lưng máy được trang bị thêm hiệu ứng lấp lánh nên khi nhìn theo góc nghiêng sẽ rất đẹp mắt. Đáng tiếc là A70 vẫn bị bám mồ hôi và dấu vân tay. Ngoài ra, viền bezel của máy cũng được làm rất mỏng, so với các máy thuộc dòng Galaxy A thì A70 là mỏng nhất.', 'SamsungGalaxyA70.jpg', '0.00'),
+(66, 'Điện thoại Samsung Galaxy M10 Ram 2GB 16GB - Hàng chính hãng', '2580000.00', 'Màn hình: PLS TFT LCD, 6.2\", HD+ • Hệ điều hành: Android 8.1 (Oreo) • Camera sau: Chính 13 MP & Phụ 5 MP • Camera trước: 5 MP • CPU: Exynos 7870 8 nhân 64-bit • RAM: 2 GB • Bộ nhớ trong: 16 GB • Thẻ nhớ: MicroSD • Thẻ SIM: 2 SIM Nano (SIM 2 chung khe thẻ nhớ) • Dung lượng pin: 3400 mAh', 'SamsungGalaxyM10.png', '0.00');
 
 -- --------------------------------------------------------
 
